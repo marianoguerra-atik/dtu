b2c60aff67f766e197eb06b0524a101ce7d42994
Mariano Guerra <mariano@marianoguerra.org>
2020-03-19 22:02:00 +0100

    vars_to_ids walker

--- src/dtu_walkers.erl
+++ src/dtu_walkers.erl
@@ -7,6 +7,7 @@
          pseq_to_tuple/3,
          lseq_to_list/3,
          mseq_to_map/3,
+         vars_to_ids/3,
          unwrap_node_id/3,
          reject_match/1]).
 
@@ -46,6 +47,12 @@ unify_vars(State0, _Path, {Type, Line, V})
     when Type =:= rvar; Type =:= lovar; Type =:= upvar ->
     {State0, {var, Line, V}};
 unify_vars(State0, _Path, Node) ->
+    {State0, Node}.
+
+vars_to_ids(State0, _Path, {Type, Line, V})
+    when Type =:= rvar; Type =:= lovar; Type =:= upvar ->
+    {State0, {id, Line, <<"$", V/binary>>}};
+vars_to_ids(State0, _Path, Node) ->
     {State0, Node}.
 
 unify_strs(State0, _Path, {Type, Line, V})
